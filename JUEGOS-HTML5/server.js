dadd8f681c7c51d2e548fcc20e9c9a0fde676365
JosueGomez99 <93223672+JosueGomez99@users.noreply.github.com>
2025-06-06 10:43:21 -0600

    update

--- JUEGOS-HTML5/server.js
+++ JUEGOS-HTML5/server.js
@@ -1,2 +1,1 @@
-// Este archivo ya no es necesario. Elimina este server.js de la subcarpeta JUEGOS-HTML5.
-// Usa solo el server.js de la raíz del proyecto para servir el juego y el WebSocket.
+// Este archivo ha sido vaciado. Usa solo el server.js de la raíz del proyecto.
